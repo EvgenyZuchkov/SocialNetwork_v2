ddea6ceaa9ae96fd13cad28219656b2cd754f39f
Evgeny Zuchkov <Evgenyzuchkov@gmail.com>
2021-02-13 22:03:44 +0300

    Layout basic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
 
 const App = () => {
-  return (
-      <div>
-        <div>First DIV</div>
-        <div>
-          <p>Second DIV</p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-            <li>REACT</li>
-          </ul>
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src="https://codesign.com.bd/conversations/content/images/2020/03/Sprint-logo-design-Codesign-agency.png"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+                </div>
+                <div>
+                    ava + discription
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default App;
