ce0499bb457651a3111cdc8f7fabf5caec28c5ad
Evgeny Zuchkov <Evgenyzuchkov@gmail.com>
2021-06-10 00:46:08 +0300

    Add avatar for Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,24 @@
 import s from './Header.module.css'
 import logo from '../../Assets/Images/logo.png'
 import {NavLink} from "react-router-dom";
+import Preloader from "../Common/Preloader/Preloader";
+
 
 const Header = (props) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
+
     return (
         <header className={s.header}>
             <img src={logo}/>
             <b>SN</b>
             <div className={s.loginBlock}>
-                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                <div className={s.infoUser}>
+                    <div>User: {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}</div>
+                    <div>Email: {props.email}</div>
+                </div>
+                <div><img src={props.profile.photos.small}/></div>
             </div>
         </header>
     )
--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -3,24 +3,28 @@ import Header from "./Header";
 import {connect} from "react-redux";
 import {getAuthUserData} from "../../Redux/Auth-reducer";
 import {compose} from "redux";
+import {getUserProfile} from "../../Redux/Profile-reducer";
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
         this.props.getAuthUserData()
+        this.props.getUserProfile(2)
     }
 
     render() {
-        return <Header {...this.props}/>
+        return <Header {...this.props} profile={this.props.profile}/>
     }
 }
 
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
+    email: state.auth.email,
+    profile: state.profilePage.profile
 })
 
 
 export default compose(
-    connect(mapStateToProps, {getAuthUserData})
+    connect(mapStateToProps, {getAuthUserData, getUserProfile})
 )(HeaderContainer);
